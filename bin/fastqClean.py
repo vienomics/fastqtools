4f2d929f28a33d072927530f30a75d6ff11ca962
deju <kdj@vienomics.com>
2018-11-01 09:53:28 +0800

    fastqClean

--- bin/fastqClean.py
+++ bin/fastqClean.py
@@ -10,7 +10,7 @@ from fastqtools.fastqReader.fastqWriter import fastqWriter
 from fastqtools.readprocess.readprocess import readprocess
 import os
 
-def readclean(read,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length):
+def readclean(read,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length,tmp_name):
     r = readprocess(read)
     r.qual(qual,qual_percent)
     r.trim(head1,tail1,head2,tail2)
@@ -18,16 +18,26 @@ def readclean(read,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_len
     r.autoadaptremove(auto_adapt)
     r.umi(umis)
     r.length(min_length)
+    fastqWriter(read,tmp_name)
     return r
 
-def main(r1,r2,prefix,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length):
+def main(r1,r2,prefix,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length,threads):
     i = 0
     filts = []
     reads = fastqReader(r1,r2)
     tmp_name = "tmp.%s" % os.getpid()
+    pools = Pool(threads)
+    ps = []
     for read in reads:
-        read = readclean(read,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length)
-        fastqWriter(read,tmp_name)
+        #read = readclean(read,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length)
+        #fastqWriter(read,tmp_name)
+        p = pools.apply_async(readclean,(read,qual,head1,tail1,head2,tail2,n_percent,autoadapt,umis,min_length,tmp_name))
+        ps.append(p)
+
+    pools.close()
+    pools.join()
+
+
     cmd = "mv %s_R1.fastq %s_R1.clean.fastq" % (tmp_name,prefix)
     os.system(cmd)
     cmd = "mv %s_R2.fastq %s_R2.clean.fastq" % (tmp_name,prefix)
@@ -42,38 +52,39 @@ if __name__ == "__main__":
 
     Options:
         #basic arguments
-        -1,--r1=<fastq>             R1 fastq         
-        -2,--r2=<fastq>             R2 fastq
-        -o,--prefix=<prefix>        ouput prefix
-
+        -1,--r1=<fastq>                  R1 fastq         
+        -2,--r2=<fastq>                  R2 fastq
+        -o,--prefix=<prefix>             ouput prefix
+        -t,--threads=<num>               threads used [default: 1]
         #quality filtering
-        --min-qual=<quality>          minimum base quality [default: 15]
-        --min-qual-percent=<num>           minimum base quality percentage [default: 0.3]
-        --max-nbase-percent=<num>    max N-base percentage [default: 0.1]
+        --min-qual=<quality>             minimum base quality [default: 15]
+        --min-qual-percent=<num>         minimum base quality percentage [default: 0.3]
+        --max-nbase-percent=<num>        max N-base percentage [default: 0.1]
         
         #length filtering
-        --min-length=<num>           minimum read length [default: 80]
+        --min-length=<num>               minimum read length [default: 80]
  
         #overlap filtering 
-        #--overlap=<num>          minimum overlapped bases [default:11]
+        #--overlap=<num>                 minimum overlapped bases [default:11]
 
         #trimming 
-        --trim1-head=<num>          trim r1 num base from R1 head [default: 0]
-        --trim1-tail=<num>          trim r1 num base from R1 tail [default: 0]
-        --trim2-head=<num>          trim r2 num base from R2 head [default: 0]
-        --trim2-tail=<num>          trim r2 num base from R2 tail [default: 0]
+        --trim1-head=<num>               trim r1 num base from R1 head [default: 0]
+        --trim1-tail=<num>               trim r1 num base from R1 tail [default: 0]
+        --trim2-head=<num>               trim r2 num base from R2 head [default: 0]
+        --trim2-tail=<num>               trim r2 num base from R2 tail [default: 0]
         
         #adapter
-        --auto-adapt-trim                auto adapt trim throgh r1/r2 overlap recommended
+        --auto-adaptor-trim              auto adapt trim throgh r1/r2 overlap recommended
 
         #umi format
-        --umi=<length|file>         umi1 length or file has umi-barcode file.
+        --umi=<length|file>              umi1 length or file has umi-barcode file.
 
     """
     args = docopt(usage)
     q1 = args["--r1"]
     q2 = args["--r2"]
     prefix = args["--prefix"]
+    threads = int(args["--threads"])
     qual = int(args["--min-qual"])
     qual_percent = float(args["--min-qual-percent"])
     n_percent = float(args["--max-nbase-percent"])
@@ -82,6 +93,6 @@ if __name__ == "__main__":
     head2 = int(args["--trim2-head"])
     tail1 = int(args["--trim1-tail"])
     tail2 = int(args["--trim2-tail"])
-    auto_adapt = args["--auto-adapt-trim"]
+    auto_adapt = args["--auto-adaptor-trim"]
     umi = args["--umi"]
-    main(q1,q2,prefix,qual,head1,tail1,head2,tail2,n_percent,auto_adapt,umi,min_length)
+    main(q1,q2,prefix,qual,head1,tail1,head2,tail2,n_percent,auto_adapt,umi,min_length,threads)
